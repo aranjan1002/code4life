af0d0b1236e6407c8908112f741b18cb68ee6cc2
aranjan1002 <anshu.ranjan1002@gmail.com>
2015-11-05 13:47:50 -0500

    All code in Progs

--- Progs/Random/test.cpp
+++ Progs/Random/test.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
-#include <cstdlib>
+#include<cstdio>
+#include<cstring>
+
 using namespace std;
 
-class test {
-  int x;
-public:
-  void print() {
-    cout << "Hello world";
-  }
-};
-
-int main()
+int main(int argc, char *argv[])
 {
-  test t;
-  t.print();
+  char    abc[27];
+  char    *ptr = abc;
+  strcpy(abc, "abcdefgxyz");
+  /*
+   * What are the types and values of expressions:
+   */
+  cout << abc << endl << *abc << &abc[3] << endl;
+  cout << abc + 4 << endl << *(abc + 5) + 20000 << endl << abc[10] << endl << abc[12] << endl << &ptr << endl;
+  /* 4. &abc[3] //&abc[3] = abc + 3*sizeof(char)
+   * 5. abc+4
+   * 6. *(abc+5) + 20000  //h  long long x = int * 1LL * int
+   * 7. abc[10] //'\0'
+   * 8. abc[12] //memset ()
+   * 9. &ptr //char**
+   */
+  return 0;
 }
-
-
-
