eb3660fc2033a18cf7074ce1dd5de770b0488e84
aranjan1002 <anshu.ranjan1002@gmail.com>
2017-02-21 00:39:52 -0500

    Wrote script to download leetcode submissions

---
+++ LeetCode/getSubbmissionsList.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+for f in $(cat temp2); do
+	curl --cookie "express.sid=s%3AHthn0HNrSz0ZE3eNKMy-7KZYznWuoeCv.UNlh6YWWvs3Wt%2BxvCi2pziA1tq1NgbJ2drMgn%2BwryZ4; LEETCODE_SESSION=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFuc2h1MiIsInVzZXJfc2x1ZyI6ImFuc2h1MiIsIl9hdXRoX3VzZXJfaWQiOiIxMTMxODUiLCJ0aW1lc3RhbXAiOiIyMDE3LTAyLTE5IDA2OjI1OjIzLjg5MjA5MyswMDowMCIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsImlkIjoxMTMxODUsIl9hdXRoX3VzZXJfaGFzaCI6ImU5MGYyN2E4MTJlMjkwNDQ4N2EzMjRiYmJkMmExNDIyZWIzZTBkNWUiLCJlbWFpbCI6ImEucmFuamFuLmNvb2xAZ21haWwuY29tIn0.6x5uWH6nnA45sJI_C9vc-7sa-mJevdjlDuFA_2zSdYo; csrftoken=sKOn3fGFGkBqiUN0gXoeAcqdqJjjMNzPQUms3Pu208fEZgSB0kvayEci98A3MLIT; _gat=1; _ga=GA1.2.200371905.1486253242; __atuvc=7%7C6%2C18%7C7%2C10%7C8; __atuvs=58aa3b78902ddc32000" $f | grep -B5 "Accepted"| grep class
+done
